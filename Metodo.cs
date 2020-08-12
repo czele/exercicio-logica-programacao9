3f71ce71b1271ee5d81ed165006c760ff625c94b
Camila Zelenovsky <camila_zele@hotmail.com>
2020-08-11 22:43:03 -0300

    first commit

---
+++ Metodo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ex9
+{
+    public class Metodo
+    {
+        public float Valido { get; set; }
+        public float Branco { get; set; }
+        public float Nulo { get; set; }
+        public float Vtotal => Valido + Branco + Nulo;
+        
+        public void Vvalido ()
+        {
+            Console.WriteLine($"O percentual de votos válidos é {(Valido / Vtotal) * 100}%.");
+        }
+
+        public void Vbranco()
+        {
+            Console.WriteLine($"O percentual de votos válidos é {(Branco / Vtotal) * 100}%.");
+        }
+
+        public void Vnulo()
+        {
+            Console.WriteLine($"O percentual de votos válidos é {(Nulo / Vtotal) * 100}%.");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ex9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Metodo metodo = new Metodo();
+
+            Console.WriteLine("Qual a quantidade de votos válidos?");
+            metodo.Valido = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Qual a quantidade de votos brancos?");
+            metodo.Branco = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Qual a quantidade de votos nulos?");
+            metodo.Nulo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Você deseja \r\n 1 - Calcular a porcentagem de votos válidos? \r\n 2 - Calcular a porcentagem de votos brancos? \r\n 3 - Calcular a porcentagem de votos nulos?");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+
+            if (input == 1)
+                metodo.Vvalido();
+            else
+            {
+
+                if (input == 2)
+                    metodo.Vbranco();
+
+                else
+                {
+
+                    if (input == 3)
+                        metodo.Vnulo();
+
+                }
+            }
+
+        }
+    }
+}
